88fc036acdd3ef94d94ff919af1fe7d52344febf
Marc Remy <marc.remy@camptocamp.com>
2011-06-30 16:46:40 +0200

    add SELinux file for PostgreSQL on RHEL6

---
+++ manifests/classes/pacemaker-postgresql.pp
@@ -0,0 +1,46 @@
+/*
+
+== Class: pacemaker::postgresql
+
+Helper which includes the postgresql::server class, but with service management
+disabled.  This is useful if PostgreSQL needs to be managed by Pacemaker, but you
+still want to benefit from the facilities provided in the PostgreSQL module.
+
+Requires:
+- postgresql's puppet module
+
+Example usage:
+  include pacemaker::postgresql
+
+*/
+class pacemaker::postgresql inherits postgresql::base {
+
+  case $operatingsystem {
+
+    RedHat,CentOS: {
+      case $lsbmajdistrelease {
+
+        "4","5": { }
+
+        default: {
+
+          selinux::module { "hapostgresql":
+            source => "puppet:///pacemaker/selinux/hapostgresql.te",
+            notify => Selmodule["hapostgresql"],
+          }
+
+          selmodule { "hapostgresql":
+            ensure => present,
+            syncversion => true,
+            require => Exec["build selinux policy package hapostgresql"],
+          }
+
+        }
+      }
+    }
+
+    default: { }
+
+  }
+
+}
