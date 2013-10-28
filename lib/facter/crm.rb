867e87f1b26fb6704037f1c1d65740753648ea32
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2013-10-28 14:06:11 +0100

    use Facter::Util::Resolution.which instead of Facter::Util::Resolution.exec

--- lib/facter/crm.rb
+++ lib/facter/crm.rb
@@ -4,7 +4,7 @@ Facter.add('crm_support') do
   confine :kernel => :linux
 
   setcode do
-    not Facter::Util::Resolution.exec('which crm_resource').empty?
+    not Facter::Util::Resolution.which('crm_resource').empty?
   end
 end
 
