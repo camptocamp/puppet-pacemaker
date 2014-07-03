d230d961bbd194256b19568e8b2d4924b75ad9ba
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2014-07-03 09:37:03 +0200

    Adapt to change in mysql module
    Use a parameter for performance setting instead of subclasses.

--- manifests/mysql/small.pp
+++
@@ -1,8 +0,0 @@
-#
-# == Class: pacemaker::mysql::small
-#
-# Use a small mysql server with pacemaker
-#
-class pacemaker::mysql::small inherits mysql::server::small {
-  include pacemaker::mysql::base
-}
--- manifests/mysql/unmanaged.pp
+++
@@ -1,8 +0,0 @@
-#
-# == Class: pacemaker::mysql::unmanaged
-#
-# Use a unmanaged mysql server with pacemaker
-#
-class pacemaker::mysql::unmanaged inherits mysql::server::unmanaged {
-  include pacemaker::mysql::base
-}
