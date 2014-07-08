30ed734d1f3012cefb1e0461f474a7cad32706ef
Marc Remy <marc.remy@camptocamp.com>
2014-07-08 09:00:36 +0200

    Add helper to open iptables with pacemaker/cman.
    Install packages on RHEL
    TODO: manage service, provider for configuration, ...

---
+++ manifests/cman.pp
@@ -0,0 +1,19 @@
+#
+# == Class: pacemaker::cman
+#
+# Install composants for using pacemaker with cman
+#
+class pacemaker::cman (
+  $corosync_mcast_address = hiera( 'corosync::multicast_address' ),
+  $corosync_mcast_port    = hiera( 'corosync::multicast_port', 5405 ),
+  $corosync_conf_template = hiera( 'corosync::conf' ),
+) {
+
+  # RHEL6.4+
+  $packages = [ 'pacemaker', 'cman', 'pcs' ]
+
+  package { $packages:
+    ensure => present,
+  }
+
+}
